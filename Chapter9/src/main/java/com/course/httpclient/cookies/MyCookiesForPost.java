5ff2858fc7f4a92c8df8555fe617239741d5c15e
jenkins <jenkins@aachuxing.com>
2019-07-11 18:02:56 +0800

    chapter9 end

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForPost.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForPost.java
@@ -2,13 +2,14 @@ package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,18 +17,16 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
-import org.apache.http.impl.client.CloseableHttpClient;
-public class MyCookiesForGet {
+
+public class MyCookiesForPost {
     private String url;
     private ResourceBundle bundle;
     private CookieStore store;
-
     @BeforeTest
     public void beforeTest() {
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);
         url = bundle.getString("test.url");
     }
-
     @Test
     public void testGetCookies() throws IOException {
         String result;
@@ -38,29 +37,43 @@ public class MyCookiesForGet {
         HttpResponse resonse = client.execute(get);
         result = EntityUtils.toString(resonse.getEntity(),"gb2312");
         System.out.println(result);
-         this.store=client.getCookieStore();
-         List<Cookie> cookieList=store.getCookies();
-         for(Cookie cookie:cookieList){
-             String name=cookie.getName();
-             String value=cookie.getValue();
-             System.out.println("cookie info: "+name+" "+value);
-         }
+        this.store=client.getCookieStore();
+        List<Cookie> cookieList=store.getCookies();
+        for(Cookie cookie:cookieList){
+            String name=cookie.getName();
+            String value=cookie.getValue();
+            System.out.println("cookie info: "+name+" "+value);
+        }
     }
     @Test(dependsOnMethods = {"testGetCookies"})
-    public void testGetWithCookies() throws IOException {
-        String uri=bundle.getString("test.get.with.cookies");
+    public void testPostWithCookies() throws IOException {
+        String uri=bundle.getString("test.post.with.cookies");
         String testUrl=this.url+uri;
-        HttpGet get=new HttpGet(testUrl);
+        /**
+         * 声明post对象
+         * 声明client
+         * 添加参数
+         * 设置cookies
+         * 设置header
+         * 添加参数到方法中
+         * 执行client
+         */
         DefaultHttpClient client=new DefaultHttpClient();
+        HttpPost post=new HttpPost(testUrl);
+        JSONObject param=new JSONObject();
+        param.put("name","huhansan");
+        param.put("age","18");
+        post.setHeader("Content-Type","application/json");
+        StringEntity entity=new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+        String result;
         client.setCookieStore(this.store);
-        HttpResponse response=client.execute(get);
-        int statusCode=response.getStatusLine().getStatusCode();
-        if(statusCode==200){
-            String result=EntityUtils.toString(response.getEntity(),"gb2312");
-            System.out.println("result: "+result);
-        }
-        System.out.println("statusCode: "+statusCode);
+        HttpResponse response=client.execute(post);
+        result=EntityUtils.toString(response.getEntity(),"utf-8");
+        JSONObject resultJson=new JSONObject(result);
+        String success=(String)resultJson.get("huhansan");
+        System.out.println("success:  "+success);
+        Assert.assertEquals("success",success);
 
     }
-
 }
